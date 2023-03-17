d15f65cd7d5c364e59f07e6a20b4552dbd515dd9
Mustafa Enes Araç <menesarac2002@gmail.com>
2023-03-17 15:45:13 +0300

    collider added

---
+++ Be Courier/Be Courier/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float hareketHizi = 10f; // aracın hızı
+    public float donmeHizi = 100f; // aracın dönüş hızı
+
+    private float yatayHareket; // sol-sağ hareket girdisi
+    private float dikeyHareket; // ileri-geri hareket girdisi
+
+    void Update()
+    {
+        yatayHareket = SimpleInput.GetAxis("Horizontal"); // sol-sağ girdisini al
+        dikeyHareket = SimpleInput.GetAxis("Vertical"); // ileri-geri girdisini al
+
+        // aracı hareket ettir
+        transform.Translate(Vector3.forward * dikeyHareket * hareketHizi * Time.deltaTime);
+        transform.Rotate(Vector3.up * yatayHareket * donmeHizi * Time.deltaTime);
+    }
+}
