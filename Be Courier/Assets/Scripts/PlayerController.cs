592e9fae2fb195b8842a2d179c03528f29bf7658
Mustafa Enes Araç <menesarac2002@gmail.com>
2023-03-24 23:53:22 +0300

    Collider Solved

--- Be Courier/Assets/Scripts/PlayerController.cs
+++ Be Courier/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float hareketHizi = 20f; // aracın hızı
     public float donmeHizi = 50f; // aracın dönüş hızı
-
+    
     private float yatayHareket; // sol-sağ hareket girdisi
     private float dikeyHareket; // ileri-geri hareket girdisi
 
+  
     void Update()
     {
         yatayHareket = SimpleInput.GetAxis("Horizontal"); // sol-sağ girdisini al
@@ -19,4 +20,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * dikeyHareket * hareketHizi * Time.deltaTime);
         transform.Rotate(Vector3.up * yatayHareket * donmeHizi * Time.deltaTime);
     }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag.Equals("Wall"))
+    //    {
+            
+    //    }
+    //}
 }
